2b6618015021f8c8f22c53a016ef56ea394156ef
Pratik Phadtare <pratikphadatare.345@gmail.com>
2024-01-21 15:27:55 +0530

    routing done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { OfficesComponent } from './offices/offices.component';
+import { NewsComponent } from './news/news.component';
+import { ContactComponent } from './contact/contact.component';
+import { AdminComponent } from './admin/admin.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", redirectTo: "/home" , pathMatch: "full"},
+  {path:"home" , component:HomeComponent},
+  {path:"about" , component:AboutComponent},
+  {path:"services" , component:ServicesComponent},
+  {path:"offices" , component:OfficesComponent},
+  {path:"news" , component:NewsComponent},
+  {path:"contact" , component:ContactComponent},
+  {path:"admin" , component:AdminComponent},
+  {path:"user" , component:UserComponent}
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { } export const
+RoutingComponent=[HomeComponent, AboutComponent,ServicesComponent,OfficesComponent,NewsComponent,ContactComponent,AdminComponent,UserComponent]
